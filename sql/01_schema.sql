93bcf5d2e5577bdd73489ba146965df672881d2e
assistenzaarena <assistenza.arena@gmail.com>
2025-08-29 23:46:59 +0200

    Update 01_schema.sql

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -1,4 +1,4 @@
--- Schema placeholder. gli useremo tra poco.
+-- Schema placeholder. li useremo tra poco.
 CREATE TABLE IF NOT EXISTS healthcheck (
   id INT AUTO_INCREMENT PRIMARY KEY,
   note VARCHAR(50) NOT NULL DEFAULT 'ok',
