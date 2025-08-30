39c3c090d47d71c41006d1c7eb43a37f732e96d8
assistenzaarena <assistenza.arena@gmail.com>
2025-08-30 11:45:31 +0200

    Update and rename header_login to header_login.php

---
+++ public/header_login.php
@@ -0,0 +1,19 @@
+<!-- Header guest: sfondo nero, logo a sinistra, azioni a destra -->
+<link rel="stylesheet" href="/assets/header_guest.css">
+
+<header class="hdr" role="banner" aria-label="Intestazione sito">
+  <div class="hdr__inner">
+    <div class="hdr__left">
+      <a class="logo" href="/" aria-label="Vai alla home">
+        <img class="logo__img" src="/assets/logo_arena.png" alt="Logo Arena" width="56" height="56">
+        <span class="logo__text">ARENA</span>
+      </a>
+    </div>
+
+    <div class="hdr__right" aria-label="Azioni account">
+      <a class="btn btn--outline" href="/home_guest.php">Esci</a>
+    </div>
+  </div>
+</header>
+  </div>
+</nav>
