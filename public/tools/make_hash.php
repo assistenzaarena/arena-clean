a5528d38ebe0f75b2c3084e5d70de57cc90c5034
assistenzaarena <assistenza.arena@gmail.com>
2025-08-31 11:47:33 +0200

    Delete public/tools directory

--- public/tools/make_hash.php
+++
@@ -1,6 +0,0 @@
-<?php
-// /tools/make_hash.php?pwd=LaTuaPasswordInChiaro
-$pwd = $_GET['pwd'] ?? '';
-header('Content-Type: text/plain; charset=utf-8');
-if ($pwd === '') { echo "Usa: ?pwd=Spiraleovale2030!"; exit; }
-echo password_hash($pwd, PASSWORD_DEFAULT);
