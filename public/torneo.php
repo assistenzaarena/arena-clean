92841d9b275d713252a78ceec7279989bcf3c115
assistenzaarena <assistenza.arena@gmail.com>
2025-09-01 12:32:09 +0200

    Update torneo.php

--- public/torneo.php
+++ public/torneo.php
@@ -319,7 +319,11 @@ function team_logo_path(string $name): string {
             $hIni  = team_initials($hn);
             $aIni  = team_initials($an);
           ?>
-          <div class="event-card" data-home-logo="<?php echo htmlspecialchars($hLogo); ?>" data-away-logo="<?php echo htmlspecialchars($aLogo); ?>">
+          <!-- (MODIFICA RICHIESTA) aggiunto data-event-id -->
+          <div class="event-card"
+               data-event-id="<?php echo (int)$ev['id']; ?>"
+               data-home-logo="<?php echo htmlspecialchars($hLogo); ?>"
+               data-away-logo="<?php echo htmlspecialchars($aLogo); ?>">
             <div class="ec-team team-side" data-side="home" title="Seleziona casa">
               <span class="logo-wrap">
                 <img class="team-logo"
