004bb12d74696d90c0efb88e237aaff29cbac0e2
assistenzaarena <assistenza.arena@gmail.com>
2025-09-06 17:20:16 +0200

    Update lobby.php

--- public/lobby.php
+++ public/lobby.php
@@ -100,6 +100,13 @@ function safeCode(array $t){
   require $_SERVER['DOCUMENT_ROOT'].'/partials/guest_mobile_drawer.php';
 ?>
 <?php
+  // Se hai una definizione condivisa della subheader guest, importala qui:
+  // $GUEST_SUBNAV = require $_SERVER['DOCUMENT_ROOT'].'/src/config/nav_guest.php'; // opzionale
+
+  require $_SERVER['DOCUMENT_ROOT'].'/partials/guest_mobile_header.php';
+  require $_SERVER['DOCUMENT_ROOT'].'/partials/guest_mobile_drawer.php';
+?>
+<?php
 $headerPath = $ROOT . '/header_user.php';
 if (file_exists($headerPath)) { require $headerPath; }
 ?>
