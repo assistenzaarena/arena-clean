a10084926eded0f1b115ab449c15d1dda4a9f239
assistenzaarena <assistenza.arena@gmail.com>
2025-09-06 17:11:49 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -26,7 +26,13 @@ require_once $ROOT . '/header_guest.php';
   </style>
 </head>
 <body>
+<?php
+  // Se hai una definizione condivisa della subheader guest, importala qui:
+  // $GUEST_SUBNAV = require $_SERVER['DOCUMENT_ROOT'].'/src/config/nav_guest.php'; // opzionale
 
+  require $_SERVER['DOCUMENT_ROOT'].'/partials/guest_mobile_header.php';
+  require $_SERVER['DOCUMENT_ROOT'].'/partials/guest_mobile_drawer.php';
+?>
   <div class="hero">
     <div>
       <h1>Benvenuti in ARENA</h1>
